d0bd62f6b821a119da04b11c87a9b823261b2e48
andremanuelbarbosa <andremanuelbarbosa@880d5450-24d9-11df-9f67-6fc5ad5a3b7e>
2012-01-02 18:59:01 +0000

    Added the first version of the PTeID4JUtils class to the project.

---
+++ core/src/main/java/pt/up/pteid4j/PTeID4JUtils.java
@@ -0,0 +1,38 @@
+package pt.up.pteid4j;
+
+/**
+ * PTeID4J Utilities Abstract Class
+ * 
+ * @author Andre Barbosa (andremanuelbarbosa@gmail.com)
+ */
+public abstract class PTeID4JUtils {
+
+  private static char[] HEXES = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+
+  /**
+   * Converts an array of bytes to an Hexadecimal String
+   * 
+   * @param bytes
+   *          the array of bytes
+   * @return the Hexadecimal String
+   */
+  public static String toHexString(byte[] bytes) {
+
+    StringBuffer stringBuffer = new StringBuffer(3 * bytes.length + 2);
+
+    for (int i = 0; i < bytes.length; i++) {
+
+      int c = bytes[i];
+
+      if ( c < 0 ) {
+
+        c += 256;
+      }
+
+      stringBuffer.append(HEXES[c / 16]);
+      stringBuffer.append(HEXES[c % 16]);
+    }
+
+    return stringBuffer.toString();
+  }
+}
