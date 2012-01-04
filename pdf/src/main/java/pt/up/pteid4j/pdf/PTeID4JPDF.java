21f88b015cf76183b85ebdb3107557dd756bd48c
andremanuelbarbosa <andremanuelbarbosa@880d5450-24d9-11df-9f67-6fc5ad5a3b7e>
2012-01-04 21:41:37 +0000

    Added the first version of the PTeID4J PDF main class to the project.

---
+++ pdf/src/main/java/pt/up/pteid4j/pdf/PTeID4JPDF.java
@@ -0,0 +1,89 @@
+package pt.up.pteid4j.pdf;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+
+import pt.up.pteid4j.PTeID4J;
+import pt.up.pteid4j.PTeID4JUtils;
+import pt.up.pteid4j.pkcs11.PTeID4JPKCS11;
+import pt.up.pteid4j.pkcs11.PTeID4JPKCS11Constants;
+import pteidlib.PteidException;
+
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.PdfDictionary;
+import com.itextpdf.text.pdf.PdfName;
+import com.itextpdf.text.pdf.PdfPKCS7;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.PdfSignature;
+import com.itextpdf.text.pdf.PdfSignatureAppearance;
+import com.itextpdf.text.pdf.PdfStamper;
+import com.itextpdf.text.pdf.PdfString;
+
+/**
+ * PTeID4J PDF Class
+ * 
+ * @author Andre Barbosa (andremanuelbarbosa@gmail.com)
+ */
+public final class PTeID4JPDF {
+
+  public PTeID4JPDF() throws Exception {
+    
+    CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
+    
+    Certificate[] certificates = new Certificate[1];
+    
+    certificates[0] = certificateFactory.generateCertificate(new FileInputStream("src/main/resources/certificates/Cartao de Cidadao 001.cer"));
+    //certificates[1] = certificateFactory.generateCertificate(PTeID4J.getSignatureCertificateCA().certif);
+    //certificates[2] = certificateFactory.generateCertificate(PTeID4J.getSignatureCertificate().certif);
+    
+    PdfReader pdfReader = new PdfReader("src/test/resources/pdf/PTeID4J.pdf");
+    FileOutputStream fout = new FileOutputStream("signed.pdf");
+    PdfStamper stp = PdfStamper.createSignature(pdfReader, fout, '\0');
+    
+    PdfSignatureAppearance sap = stp.getSignatureAppearance();
+    sap.setCrypto(null, certificates, null, PdfSignatureAppearance.SELF_SIGNED);
+    sap.setReason("I'm the author");
+    sap.setLocation("Lisbon");
+    sap.setVisibleSignature(new Rectangle(100, 100, 200, 200), 1, null);
+    sap.setExternalDigest(new byte[128], null, "RSA");
+    sap.preClose();
+    
+    PdfPKCS7 sig = sap.getSigStandard().getSigner();
+
+    byte[] content = PTeID4JUtils.toByteArray(sap.getRangeStream());
+
+    //byte[] hash = MessageDigest.getInstance("SHA-1").digest(content);
+
+    //byte[] signatureBytes = scd.generateNonRepudiationSignature(hash);
+    byte[] signatureBytes = PTeID4J.sign(content, PTeID4JPKCS11Constants.DIGEST_INFO_PREFIX_SHA1);
+
+    sig.setExternalDigest(signatureBytes, new byte[20], "RSA");
+    PdfDictionary dic = new PdfDictionary();
+    dic.put(PdfName.CONTENTS, new PdfString(sig.getEncodedPKCS1()).setHexWriting(true));
+    sap.close(dic);
+    
+    
+    stp.close();
+    
+    fout.close();
+    pdfReader.close();
+  }
+  
+  /**
+   * @param args
+   * @throws Exception 
+   * @throws KeyStoreException 
+   */
+  public static void main(String[] args) throws Exception {
+
+    new PTeID4JPDF();
+  }
+}
