b2a7445ea8f5d574924edb1f32e9896dd21df1c6
andremanuelbarbosa@gmail.com <andremanuelbarbosa@gmail.com@880d5450-24d9-11df-9f67-6fc5ad5a3b7e>
2012-01-09 17:57:19 +0000

    Added the first version of the PTeID4J ID Applet main class.

---
+++ id-applet/src/main/java/pt/up/pteid4j/id/applet/PTeID4JIDApplet.java
@@ -0,0 +1,20 @@
+package pt.up.pteid4j.id.applet;
+
+import java.applet.Applet;
+import java.awt.Graphics;
+
+/**
+ * PTeID4J ID Applet Class
+ * 
+ * @author Andre Barbosa (andremanuelbarbosa@gmail.com)
+ */
+public class PTeID4JIDApplet extends Applet {
+
+  private static final long serialVersionUID = -6042565559792013278L;
+
+  @Override
+  public void paint(Graphics graphics) {
+    
+    
+  }
+}
