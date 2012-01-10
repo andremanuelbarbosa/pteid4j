7570f17c1def478f81a3e837ca4d0c8af2bfc001
andremanuelbarbosa <andremanuelbarbosa@880d5450-24d9-11df-9f67-6fc5ad5a3b7e>
2012-01-10 01:08:18 +0000

    Modified the PTeID4J ID Applet to return the Citizen Name using JNLP.

--- id-applet/src/main/java/pt/up/pteid4j/id/applet/PTeID4JIDApplet.java
+++ id-applet/src/main/java/pt/up/pteid4j/id/applet/PTeID4JIDApplet.java
@@ -1,20 +1,77 @@
 package pt.up.pteid4j.id.applet;
 
-import java.applet.Applet;
 import java.awt.Graphics;
+import java.lang.reflect.Constructor;
+import java.security.Permission;
+
+import javax.swing.JApplet;
+
+import pt.up.pteid4j.id.PTeID4JID;
+import pteidlib.PteidException;
 
 /**
  * PTeID4J ID Applet Class
  * 
  * @author Andre Barbosa (andremanuelbarbosa@gmail.com)
  */
-public class PTeID4JIDApplet extends Applet {
+public class PTeID4JIDApplet extends JApplet {
 
   private static final long serialVersionUID = -6042565559792013278L;
 
+  public String getName() {
+
+    try {
+
+      return PTeID4JID.getForename() + " " + PTeID4JID.getSurname();
+
+    } catch (PteidException e) {
+
+      e.printStackTrace();
+
+      return null;
+    }
+  }
+
+  @Override
+  public void start() {
+
+    SecurityManager securityManager = System.getSecurityManager();
+
+    if ( securityManager != null ) {
+
+      try {
+
+        Object securityContext = securityManager.getSecurityContext();
+
+        Class<?> cardPermissionClass = Class.forName("javax.smartcardio.CardPermission");
+        Constructor<?> cardPermissionConstructor = cardPermissionClass.getConstructor(String.class, String.class);
+        Permission permission = (Permission) cardPermissionConstructor.newInstance("*", "*");
+        securityManager.checkPermission(permission, securityContext);
+
+        PTeID4JID.init();
+
+      } catch (Exception e) {
+
+        e.printStackTrace();
+      }
+    }
+  }
+
   @Override
   public void paint(Graphics graphics) {
-    
-    
+
+  }
+
+  @Override
+  public void stop() {
+
+    try {
+
+      PTeID4JID.terminate();
+
+    } catch (PteidException e) {
+
+      e.printStackTrace();
+    }
   }
 }
